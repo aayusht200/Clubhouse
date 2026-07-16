1ee2b2fb19a33d55f3417436bb7da31385c0cfbf
Aayush Trivedi <aayush.t200@gmail.com>
2026-07-16 17:04:18 +0530

    fix: refactor database connection to use DATABASE_URL and remove commented-out code

--- server/DB/connection.js
+++ server/DB/connection.js
@@ -4,10 +4,17 @@ pg.types.setTypeParser(1082, (val) => val); //disables autoconversion of certain
 if (process.env.NODE_ENV !== 'production') {
     await import('dotenv/config');
 }
+
 const pool = new pg.Pool({
-    user: process.env.user,
-    password: process.env.password,
-    host: process.env.host,
-    database: process.env.database,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
+// const pool = new pg.Pool({
+//     user: process.env.user,
+//     password: process.env.password,
+//     host: process.env.host,
+//     database: process.env.database,
+// });
 export { pool };
