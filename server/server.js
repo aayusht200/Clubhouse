0ee29543bf48dc54a05e87733652e991bab0b1c0
Aayush Trivedi <aayush.t200@gmail.com>
2026-07-16 16:32:51 +0530

    fix: update server scripts in package.json and load environment variables conditionally in server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-process.loadEnvFile();
-
+if (process.env.NODE_ENV !== 'production') {
+    await import('dotenv/config');
+}
 import { app } from './app.js';
 
 const port = process.env.PORT || 3000;
